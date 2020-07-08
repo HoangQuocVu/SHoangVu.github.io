eecc890dccffd321e9ab0e4f8cba48260241efcc
SHoangVu <hoangquocvu2000@gmail.com>
2020-07-09 01:21:16 +0700

    Finsh-project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter as Router,Route } from 'react-router-dom';
+import './App.css';
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+import Landing from './components/home/Landing';
+
+
+import store from './store';
+import Movie  from './components/home/Movie';
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/movie/:id" component={Movie}/>
+          <Footer />
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/components/home/Landing.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import {connect} from 'react-redux';
+
+import SearchForm from './SearchForm';
+import Spinner from '../layout/Spinner';
+import MoviesContainer from './MoviesContainer';
+
+export class Landing extends Component {
+    render() {
+        const {loading} =this.props;
+
+        return (
+            <div className="container">
+                <SearchForm />
+                {loading ? <Spinner /> : <MoviesContainer />} 
+            </div> //đúng thì hiện thị spinner và sai thì phim nhập
+        );
+    }
+}
+
+
+const mapStateToProps = state => ({
+    loading: state.movies.loading
+})
+
+export default connect(mapStateToProps)(Landing);
